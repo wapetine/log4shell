14c4e68967dc6850e687fb31f3744131d44320fc
wapetine <97812933+wapetine@users.noreply.github.com>
2022-01-16 01:11:52 +0100

    Update log4jscan.sh

--- log4jscan.sh
+++ log4jscan.sh
@@ -1,3 +1,13 @@
-tcpserver -v -1 0.0.0.0 1234 sh -c "echo 'Vulnerable machine'" 2> vulnerable.txt
+echo "Starting server"
 
-nmap --script=http-log4shell,ssh-log4shell,imap-log4shell  '--script-args=log4shell.payload="${jndi:ldap://172.17.0.1:1234}"' -T4 -n -p8080-8081 -sV --script-timeout=1m 172.17.0.2-3
+tcpserver -v 0.0.0.0 1234 sh -c "echo 'Vulnerable machine'" 2> vulnerable.txt &
+
+echo "Scanning hosts..."
+
+nmap --script=http-log4shell,ssh-log4shell,imap-log4shell  '--script-args=log4shell.payload="${jndi:ldap://172.17.0.1:1234}"' -T4 -n -p$2 -sV --script-timeout=1m $1
+
+echo "\n"
+echo "\n"
+echo "List of vulnerable hosts"
+echo "========================"
+cat vulnerable.txt | grep -a from | grep -Po '[0-9.]{7,15}'
